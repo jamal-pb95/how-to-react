48ee678ad3003621e1f3fee9a749e06ac917390f
jamal-pb95 <jamal.pb95@gmail.com>
2018-10-25 13:16:11 +0600

    init

--- react-bootstrap/reactstrap/src/App.js
+++ react-bootstrap/reactstrap/src/App.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-class App extends Component {
+class ThemeSwitcher extends Component {
+
+  state = { theme: null, dropdownOpen: false }
+  
+  toggleDropdown = () => {
+    this.setState({ dropdownOpen: !this.state.dropdownOpen });
+  }
+  
+  resetTheme = evt => {
+    evt.preventDefault();
+    this.setState({ theme: null });
+  }
+  
+  chooseTheme = (theme, evt) => {
+    evt.preventDefault();
+    this.setState({ theme });
+  }
+  
   render() {
+  
+    const { theme, dropdownOpen } = this.state;
+    const themeClass = theme ? theme.toLowerCase() : 'secondary';
+    
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="d-flex flex-wrap justify-content-center position-absolute w-100 h-100 align-items-center align-content-center">
+      
+        <span className={`h1 mb-4 w-100 text-center text-${themeClass}`}>{theme || 'Default'}</span>
+        
+        <ButtonDropdown isOpen={dropdownOpen} toggle={this.toggleDropdown}>
+          <Button id="caret" color={themeClass}>{theme || 'Custom'} Theme</Button>
+          <DropdownToggle caret size="lg" color={themeClass} />
+          <DropdownMenu>
+            <DropdownItem onClick={e => this.chooseTheme('Primary', e)}>Primary Theme</DropdownItem>
+            <DropdownItem onClick={e => this.chooseTheme('Danger', e)}>Danger Theme</DropdownItem>
+            <DropdownItem onClick={e => this.chooseTheme('Success', e)}>Success Theme</DropdownItem>
+            <DropdownItem divider />
+            <DropdownItem onClick={this.resetTheme}>Default Theme</DropdownItem>
+          </DropdownMenu>
+        </ButtonDropdown>
+        
       </div>
     );
+    
   }
+  
 }
 
-export default App;
+export default ThemeSwitcher;
